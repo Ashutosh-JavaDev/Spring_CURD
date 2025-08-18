5e25938dbd65501043970efb449273efb1497a69
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-19 02:25:57 +0530

    Initial commit

---
+++ src/test/java/com/CURD_Operations/CURD/CurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CURD_Operations.CURD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
