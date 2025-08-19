e627b9614fa04d85597707bcaa47020df10faccd
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-19 15:08:08 +0530

    Reset Code Commit

---
+++ src/main/java/com/CURD_Operations/CURD/EmployeeInfo.java
@@ -0,0 +1,11 @@
+package com.CURD_Operations.CURD;
+
+import lombok.Data;
+
+@Data
+public class EmployeeInfo {
+    private Long ID;
+    private String Name;
+    private String Email;
+    private Long phone;
+}
