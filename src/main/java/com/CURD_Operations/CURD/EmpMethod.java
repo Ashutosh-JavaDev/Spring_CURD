f820cdd4f6ad1132df466bfa1468a8222ef117cf
Ashutosh Ayush <jbnashu297@gmail.com>
2025-08-20 00:30:27 +0530

    Remove Autowired and created a constructor

--- src/main/java/com/CURD_Operations/CURD/EmpMethod.java
+++ src/main/java/com/CURD_Operations/CURD/EmpMethod.java
@@ -1,10 +1,7 @@
 package com.CURD_Operations.CURD;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
